18637c6cadd043aead7b2ca0e73e0f9e378d0686
bhargavvijay <141197189+bhargavvijay@users.noreply.github.com>
2024-09-15 19:02:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin:  'https://quadb-assignment-bhargav.netlify.ap'
+    origin:  'https://quadb-assignment-bhargav.netlify.app/'
 }));
 
 sequelize.sync({ force: false }).then(() => {
